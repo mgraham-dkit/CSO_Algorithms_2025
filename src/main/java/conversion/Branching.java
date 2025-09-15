f1e5858a8e5925dbba374685f8f4c59c7903ac91
michelle <michelle.graham@dkit.ie>
2025-09-15 16:33:11 +0100

    Completed branching example

--- src/main/java/conversion/Branching.java
+++ src/main/java/conversion/Branching.java
@@ -1,4 +1,26 @@
 package conversion;
 
+import java.util.Scanner;
+
 public class Branching {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int value = 20;
+
+        System.out.println("Welcome to the number guessing game!");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Enter a number between 1 and 50: ");
+            int guess = input.nextInt();
+
+            if (guess == value) {
+                System.out.println("Correct!");
+                break;
+            } else if (guess < value) {
+                System.out.println("Too low");
+            } else {
+                System.out.println("Too high!");
+            }
+        }
+    }
 }

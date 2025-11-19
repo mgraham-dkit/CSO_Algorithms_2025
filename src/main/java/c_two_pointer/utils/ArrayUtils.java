aa14d187aa0e2757e2c087924fecbce2d761d105
michelle <michelle.graham@dkit.ie>
2025-11-19 15:49:49 +0000

    feat: sorting functionality
    - bubble sort (for strings and ints)
    - optimised bubble sort (for ints)
    - selection sort (for ints)

--- src/main/java/c_two_pointer/utils/ArrayUtils.java
+++ src/main/java/c_two_pointer/utils/ArrayUtils.java
@@ -55,4 +55,98 @@ public class ArrayUtils {
 
         return sb.toString();
     }
+
+    public static void selectionSort(int [] nums){
+        validateNullArray(nums);
+
+        for(int startSlot = 0; startSlot < nums.length-1; startSlot++){
+            int minIndex = startSlot;
+
+            minIndex = getMinIndex(nums, startSlot+1, minIndex);
+
+            swap(nums, startSlot, minIndex);
+        }
+    }
+
+    public static int getMinIndex(int[] nums, int startSlot, int minIndex) {
+        for (int i = startSlot; i < nums.length; i++) {
+            if(nums[i] < nums[minIndex]){
+                minIndex = i;
+            }
+        }
+        return minIndex;
+    }
+
+    public static void optimisedSort(int [] data){
+        validateNullArray(data);
+
+        if(data.length <= 1){
+            return;
+        }
+
+        for (int passCount = 0; passCount < data.length; passCount++) {
+            boolean swapped = false;
+            for (int i = 0; i < data.length - 1 - passCount; i++) {
+                if(data[i] > data[i+1]){
+                    swap(data, i, i+1);
+                    swapped = true;
+                }
+            }
+            if(!swapped){
+                break;
+            }
+        }
+    }
+
+    public static void sort(int [] data){
+        validateNullArray(data);
+
+        if(data.length <= 1){
+            return;
+        }
+
+        for (int passCount = 0; passCount < data.length; passCount++) {
+            for (int i = 0; i < data.length - 1 - passCount; i++) {
+                if(data[i] > data[i+1]){
+                    swap(data, i, i+1);
+                }
+            }
+        }
+    }
+
+    private static void validateNullArray(int[] data) {
+        if(data == null){
+            throw new IllegalArgumentException("Array to be sorted cannot be null");
+        }
+    }
+
+    public static void sort(String [] data){
+        if(data == null){
+            throw new IllegalArgumentException("Array to be sorted cannot be null");
+        }
+
+        if(data.length <= 1){
+            return;
+        }
+
+        for (int passCount = 0; passCount < data.length; passCount++) {
+            for (int i = 0; i < data.length - 1 - passCount; i++) {
+                if(data[i].compareToIgnoreCase(data[i+1]) > 0){
+                    swap(data, i, i+1);
+                }
+            }
+        }
+    }
+
+    private static void swap(String[] data, int posA, int posB) {
+        String temp = data[posA];
+        data[posA] = data[posB];
+        data[posB] = temp;
+    }
+
+    private static void swap(int[] data, int posA, int posB) {
+        int temp = data[posA];
+        data[posA] = data[posB];
+        data[posB] = temp;
+    }
 }
